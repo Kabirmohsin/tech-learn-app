978c2687a41d286d7b2a134810cbffaa0f74aa32
Kabir Mohsin <kabirmohsin892@gmail.com>
2026-02-21 17:13:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import certificateRoutes from "./routes/certificateRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/certificates", certificateRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
